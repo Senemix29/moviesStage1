1b25ab25a6956f47c5ce4c816dd6b566331e081b
Natan Ximenes <natanximenes29@gmail.com>
2018-07-22 19:12:43 -0300

    Initial commit with network layer implemented

---
+++ app/src/main/java/br/com/natanximenes/stage1/Movie.java
@@ -0,0 +1,64 @@
+package br.com.natanximenes.stage1;
+
+public class Movie {
+    public static final String TITLE_PROP = "title";
+    public static final String MOVIE_POSTER_PROP = "poster_path";
+    public static final String SYNOPSIS_PROP = "overview";
+    public static final String USER_RATING_PROP = "vote_average";
+    public static final String RELEASE_DATE = "release_date";
+    
+    private String title;
+    private String moviePoster;
+    private String synopsis;
+    private Double userRating;
+    private String releaseDate;
+
+    public Movie(String title, String moviePoster, String synopsis, Double userRating, String
+            releaseDate) {
+        this.title = title;
+        this.moviePoster = moviePoster;
+        this.synopsis = synopsis;
+        this.userRating = userRating;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMoviePoster() {
+        return moviePoster;
+    }
+
+    public void setMoviePoster(String moviePoster) {
+        this.moviePoster = moviePoster;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Double getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(Double userRating) {
+        this.userRating = userRating;
+    }
+}
---
+++ app/src/main/java/br/com/natanximenes/stage1/utils/MovieJsonUtils.java
@@ -0,0 +1,55 @@
+package br.com.natanximenes.stage1.utils;
+
+import android.support.annotation.Nullable;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.natanximenes.stage1.Movie;
+
+import static br.com.natanximenes.stage1.Movie.MOVIE_POSTER_PROP;
+import static br.com.natanximenes.stage1.Movie.RELEASE_DATE;
+import static br.com.natanximenes.stage1.Movie.SYNOPSIS_PROP;
+import static br.com.natanximenes.stage1.Movie.TITLE_PROP;
+import static br.com.natanximenes.stage1.Movie.USER_RATING_PROP;
+
+public class MovieJsonUtils {
+    private static final String ERROR_PROP = "status_code";
+    private static final String MOVIES_PROP = "results";
+
+    @Nullable
+    public static List<Movie> getMovieListFromJson(String jsonString) throws JSONException {
+        final List<Movie> movieList = new ArrayList<>();
+        final JSONObject moviesJSON = new JSONObject(jsonString);
+
+        if (moviesJSON.has(ERROR_PROP)) {
+            return null;
+        }
+
+        final JSONArray moviesJSONArray = moviesJSON.getJSONArray(MOVIES_PROP);
+
+        if (moviesJSONArray == null) {
+            return null;
+        }
+
+        for (int i = 0; i < moviesJSONArray.length(); i++) {
+            final JSONObject movieJSONObject = moviesJSONArray.getJSONObject(i);
+            if (movieJSONObject == null){
+                return null;
+            }
+
+            movieList.add(new Movie(
+                    movieJSONObject.getString(TITLE_PROP),
+                    movieJSONObject.getString(MOVIE_POSTER_PROP),
+                    movieJSONObject.getString(SYNOPSIS_PROP),
+                    movieJSONObject.getDouble(USER_RATING_PROP),
+                    movieJSONObject.getString(RELEASE_DATE)));
+        }
+
+        return movieList;
+    }
+}
